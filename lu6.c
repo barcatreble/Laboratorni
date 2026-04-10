661bd445bbdcc00f1e22dc7dd71359f9481c1002
barcatreble <nikola.ap06@gmail.com>
2026-04-10 19:50:29 +0300

    Add files via upload

--- lu6.c
+++ lu6.c
@@ -142,5 +142,37 @@ int main()
     free(arr2);
     free(arr3);*/
 
-    //5
+    //5 Pascal
+    int n = 4;
+    int** arr = (int**)malloc(n * sizeof(int*));
+    if (arr == NULL) return 1;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = (int*)malloc((i+1) * sizeof(int));
+        if(arr[i] == NULL) return 1;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            if(j == 0 || j == i) arr[i][j] = 1;
+            else arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("%d", arr[i][j]);
+        }
+        printf("/n");
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        free(arr[i]);
+    }
+    free(arr);
 }
